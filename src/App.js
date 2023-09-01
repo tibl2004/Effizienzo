a48292f953dc290bda25cd9463338ba419455e8f
Bernhardiner07 <timo.blumer@gmx.ch>
2023-09-01 10:25:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { FaCopy, FaCheck } from 'react-icons/fa';
 import './App.css';
+import logo from '../src/Logo.png'; 
 
 function App() {
   const [data, setData] = useState({
@@ -13,35 +15,66 @@ function App() {
     Lieferant: ''
   });
 
-  const handleChange = (label, value) => {
+  const [copiedLabel, setCopiedLabel] = useState(null);
+
+  const handleInputChange = (label, value) => {
     setData(prevData => ({
       ...prevData,
       [label]: value
     }));
   };
 
-  const handleCopy = () => {
-    const text = Object.entries(data)
-      .map(([label, value]) => `${label}: ${value}`)
-      .join('\n');
-    navigator.clipboard.writeText(text);
+  const handleCopy = (label) => {
+    const text = `${label}: ${data[label]}`;
+
+    try {
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+
+      setCopiedLabel(label);
+      setTimeout(() => setCopiedLabel(null), 1000);
+    } catch (error) {
+      console.error('Kopieren fehlgeschlagen:', error);
+    }
   };
 
   return (
     <div className="App">
-      <div>
-        {Object.entries(data).map(([label, value]) => (
-          <div key={label} className="input-container">
-            <label>{label}:</label>
-            <input
-              type="text"
-              value={value}
-              onChange={e => handleChange(label, e.target.value)}
-            />
-          </div>
-        ))}
+      <div className='navbar'>
+        <div className='logo-container'>
+          <img src={logo} alt="Restwert" />
+        </div>
       </div>
-      <button onClick={handleCopy}>Inhalt kopieren</button>
+      {Object.entries(data).map(([label, value]) => (
+        <div key={label} className="input-container">
+          <label style={{ fontSize: '18px', marginRight: '10px' }}>{label}:</label>
+          <input
+            type="text"
+            value={value}
+            onChange={e => handleInputChange(label, e.target.value)}
+            style={{
+              width: '200px',
+              height: '30px',
+              fontSize: '20px', // Increase font size
+            }}
+          />
+          <button
+            onClick={() => handleCopy(label)}
+            style={{
+              backgroundColor: copiedLabel === label ? 'limegreen' : '',
+              height: '40px',
+              marginLeft: '10px',
+              fontSize: '20px', // Increase font size
+            }}
+          >
+            {copiedLabel === label ? <FaCheck /> : <FaCopy />}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
