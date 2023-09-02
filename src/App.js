9f3de6770f1f0cf3623d88531b12928efe60d5b7
Bernhardiner07 <timo.blumer@gmx.ch>
2023-09-03 00:38:33 +0200

    fix center

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { FaCopy, FaCheck } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy, faCheck, faTrash } from '@fortawesome/free-solid-svg-icons'; // Importiere die Icons
+
 import './App.css';
-import logo from '../src/Logo.jpeg'; 
+import logo from '../src/Logo.jpeg';
 
 function App() {
   const [data, setData] = useState({
@@ -41,6 +43,18 @@ function App() {
     }
   };
 
+  const handleClearAll = () => {
+    setData({
+      Marke: '',
+      Grösse: '',
+      Anzahl: '',
+      Material: '',
+      Farbe: '',
+      Schäden: '',
+      Hinweise: '',
+    });
+  };
+
   return (
     <div className="App">
       <div className='navbar'>
@@ -48,32 +62,39 @@ function App() {
           <img src={logo} alt="Restwert" />
         </div>
       </div>
-      {Object.entries(data).map(([label, value]) => (
-        <div key={label} className="input-container">
-          <label style={{ fontSize: '18px', marginRight: '10px' }}>{label}:</label>
-          <input
-            type="text"
-            value={value}
-            onChange={e => handleInputChange(label, e.target.value)}
-            style={{
-              width: '200px',
-              height: '30px',
-              fontSize: '20px', // Increase font size
-            }}
-          />
-          <button
-            onClick={() => handleCopy(label)}
-            style={{
-              backgroundColor: copiedLabel === label ? 'limegreen' : '',
-              height: '40px',
-              marginLeft: '10px',
-              fontSize: '20px', // Increase font size
-            }}
-          >
-            {copiedLabel === label ? <FaCheck /> : <FaCopy />}
-          </button>
-        </div>
-      ))}
+      <div className="input-container">
+        {Object.entries(data).map(([label, value]) => (
+          <div key={label} className="input-field">
+            <label style={{ fontSize: '18px', marginRight: '10px' }}>{label}:</label>
+            <input
+              type="text"
+              value={value}
+              onChange={e => handleInputChange(label, e.target.value)}
+            />
+            <button
+              onClick={() => handleCopy(label)}
+              style={{
+                backgroundColor: copiedLabel === label ? 'limegreen' : '',
+              }}
+            >
+              {copiedLabel === label ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faCopy} />}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="center-button">
+        <button
+          onClick={handleClearAll}
+          style={{
+            backgroundColor: 'red',
+            color: 'white',
+          }}
+        >
+          <span className="trash-icon">
+            <FontAwesomeIcon icon={faTrash} />
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
