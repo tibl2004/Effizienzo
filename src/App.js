fb4f1daa02d28df0b2d4606d1d83235cd6dd22ed
Bernhardiner07 <timo.blumer@gmx.ch>
2023-09-05 19:44:45 +0200

    added 2. Reaktivierung

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     'Bei Nichtverkauf': '',
     'Startpreis': '',
     '1. Reaktivierung': '', // 1. Reaktivierung als leeres Feld
+    '2. Reaktivierung': '',
   });
 
   const [selectedOption, setSelectedOption] = useState('');
@@ -43,11 +44,21 @@ function App() {
     }
   };
 
+  const handleReaktivierung2Change = (label, value) => {
+    // Überprüfe, ob der eingegebene Wert nur Zahlen und "." enthält, und aktualisiere das Datenobjekt entsprechend.
+    if (/^[0-9.]*$/.test(value) || label !== '2. Reaktivierung') {
+      setData((prevData) => ({
+        ...prevData,
+        [label]: value,
+      }));
+    }
+  };
+
   const handleCopy = (label) => {
     let text = `${label}: ${data[label]}`;
 
     // Wenn der Kopieren-Button für 'Startpreis' oder '1. Reaktivierung' geklickt wird, füge 'CHF ' vor der Zahl hinzu
-    if (label === 'Startpreis' || label === '1. Reaktivierung') {
+    if (label === 'Startpreis' || label === '1. Reaktivierung' || label === '2. Reaktivierung') {
       text = `CHF ${data[label]}`;
     }
 
@@ -97,6 +108,7 @@ function App() {
       'Bei Nichtverkauf': '',
       'Startpreis': '',
       '1. Reaktivierung': '',
+      '2. Reaktivierung': '',
     });
   };
 
@@ -160,9 +172,19 @@ function App() {
             ) : (
               <>
                 <input
-                  type={(label === 'Startpreis' || label === '1. Reaktivierung') ? 'text' : 'text'}
+                  type={(label === 'Startpreis' || label === '1. Reaktivierung' || label === '2. Reaktivierung') ? 'text' : 'text'}
                   value={value}
-                  onChange={(e) => (label === 'Startpreis' ? handleInputChange(label, e.target.value) : handleReaktivierungChange(label, e.target.value))}
+                  onChange={(e) => {
+                    if (label === 'Startpreis') {
+                      handleInputChange(label, e.target.value);
+                    } else if (label === '1. Reaktivierung') {
+                      handleReaktivierungChange(label, e.target.value);
+                    } else if (label === '2. Reaktivierung') {
+                      handleReaktivierung2Change(label, e.target.value);
+                    } else {
+                      handleInputChange(label, e.target.value);
+                    }
+                  }}
                 />
                 <button
                   onClick={() => handleCopy(label)}
