93b6de562a3a507316fd986f76f3ed6fcea44c87
Bernhardiner07 <timo.blumer@gmx.ch>
2023-09-14 09:10:47 +0200

    test

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Routes>
           <Route
-            path="/"
+            path="/Effizienzo"
             element={
               <>
                 <Navigation />
