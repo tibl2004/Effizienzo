5c8c57761fe75eed416254cfb4fe94da44817bd5
Bernhardiner07 <timo.blumer@gmx.ch>
2023-11-13 11:23:02 +0100

    w

--- src/Versand/Versand.jsx
+++ src/Versand/Versand.jsx
@@ -11,25 +11,31 @@ function Versand() {
         { id: 6, format: '', dicke: '', gewicht: '501 - 1000 g', porto: 'CHF 3.65', faktura: 'CHF 4.05' },
     ];
 
+    const pakete = [
+        { id: 1, masse: 'Bis 60.0 cm x 40.0 cm x 41.0 cm', gewicht: 'Bis 2 kg', priceStandard: 'CHF 8.50', priceExtra: 'CHF 12.50' },
+        { id: 2, masse: '', gewicht: 'Bis 5 kg', priceStandard: 'CHF 10.00', priceExtra: 'CHF 14.00' },
+        { id: 3, masse: '', gewicht: 'Bis 10 kg', priceStandard: 'CHF 12.00', priceExtra: 'CHF 16.00' },
+        { id: 4, masse: '', gewicht: 'Bis 30 kg', priceStandard: 'CHF 15.00', priceExtra: 'CHF 19.00' },
+    ];
+
     const [copiedId, setCopiedId] = useState(null);
 
     const handleCopy = (id, text) => {
         const [, amount] = text.split(' '); // Splitten nach Leerzeichen und nur die zweite Hälfte (Betrag) verwenden
-    
+
         const tempInput = document.createElement('input');
         tempInput.value = amount;
         document.body.appendChild(tempInput);
         tempInput.select();
         document.execCommand('copy');
         document.body.removeChild(tempInput);
-    
+
         setCopiedId(id);
-    
+
         setTimeout(() => {
             setCopiedId(null);
         }, 3000);
     };
-    
 
     return (
         <div>
@@ -66,6 +72,37 @@ function Versand() {
 
             <p>* Dicke = Kuvert + Inhalt</p>
             <p>** Faktura-Preis = Portokosten Post + Zuschlag für Verpackungsmaterial</p>
+
+            <h2>Pakete</h2>
+
+
+
+
+            <table border="1" className='pakete-table'>
+                <thead>
+                    <tr>
+                        <th>Masse</th>
+                        <th>Gewicht</th>
+                        <th>Preis Standard</th>
+                        <th>Preis Extra</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pakete.map((paket, index) => (
+                        <tr key={paket.id}>
+                            {index === 0 ? (
+                                <>
+                                    <td rowSpan={pakete.length}>{paket.masse}</td>
+                                </>
+                            ) : null}
+                            <td>{paket.gewicht}</td>
+                            <td>{paket.priceStandard}</td>
+                            <td>{paket.priceExtra}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
         </div>
     );
 }
