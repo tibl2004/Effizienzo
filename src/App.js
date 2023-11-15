3e6b7578d5342050c92d6a6359c717b0a093bffd
Bernhardiner07 <timo.blumer@gmx.ch>
2023-11-15 17:04:45 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
       <Router>
         <Routes>
         <Route
-            path="/Effzienzo"
+            path="/Effizienzo"
             element={
               
                 <Startsite />
