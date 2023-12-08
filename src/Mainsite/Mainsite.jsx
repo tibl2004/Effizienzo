a509a84b6fac74fe9df388fab0ec03634f6f224b
unknown <timo.blumer@gewa.ch>
2023-12-08 15:12:51 +0100

    update

--- src/Mainsite/Mainsite.jsx
+++ src/Mainsite/Mainsite.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import ReactStars from 'react-rating-stars-component';
 import './Mainsite.scss';
 
 function Mainsite() {
   const [updates, setUpdates] = useState([]);
   const [username, setUsername] = useState('');
+  const [userRating, setUserRating] = useState(0);
 
   const location = useLocation();
 
@@ -33,11 +35,16 @@ function Mainsite() {
     return `${year}-${month}-${day}`;
   };
 
+  // Callback function when the user rates
+  const handleRatingChange = (newRating) => {
+    setUserRating(newRating);
+    // Hier könntest du den Bewertungswert an einen Server senden oder lokal speichern
+  };
+
   return (
     <div className="container">
       <div className="welcome-container">
         <h2 className="hello-message">Lieber {username}</h2>
-        {/* Neuer Code: Muster-Text oder Lückenfüller-Text */}
         <p className="muster-text">Herzlich willkommen auf Effizienzo! Schön, dass du dich entschieden hast, meine Plattform zu benutzen. Viel Spaß und bei Fragen komm einfach auf mich zu!</p>
         <p className='muster-text'>Liebe Grüße, Timo Blumer</p>
       </div>
@@ -48,10 +55,25 @@ function Mainsite() {
           {updates.map((update, index) => (
             <React.Fragment key={update.id}>
               <Update datum={update.datum} title={update.title} text={update.text} />
-              {index !== updates.length - 1 && <hr />} {/* Display horizontal line if not the last update */}
+              {index !== updates.length - 1 && <hr />}
             </React.Fragment>
           ))}
         </div>
+      </div>
+
+      {/* Feedback-Widget */}
+      <div className='feedback-container'>
+        <h3>Wie gefällt dir Effizienzo?</h3>
+        <ReactStars
+          count={5}
+          onChange={handleRatingChange}
+          size={30}
+          value={userRating}
+          isHalf={false}
+          emptyIcon={<i className="far fa-star"></i>}
+          fullIcon={<i className="fas fa-star"></i>}
+          activeColor="#ffd700"
+        />
       </div>
     </div>
   );
