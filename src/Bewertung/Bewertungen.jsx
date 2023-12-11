a91394fe96be07c99f77ca4cb6a40133c7ffc4d8
unknown <timo.blumer@gewa.ch>
2023-12-11 13:08:02 +0100

    update

--- src/Bewertung/Bewertungen.jsx
+++ src/Bewertung/Bewertungen.jsx
@@ -3,7 +3,15 @@ import { FaRegSmile, FaMeh, FaRegFrown } from 'react-icons/fa';
 import './Bewertungen.scss';
 
 function Bewertungen() {
-  const [greetingMessage, setGreetingMessage] = useState("Alles super. Vielen Dank und gerne wieder!");
+  const bewertungen = [
+    "Alles super. Vielen Dank und gerne wieder!",
+    "Besten Dank für Ihren Kauf gerne wieder",
+    "Vielen dank für Ihren Kauf bei uns.",
+    "Was alles so Tiptop.",
+    "Hat wie immer alles wunderbar geklappt-Herzlichen Dank!"
+  ];
+
+  const [greetingMessage, setGreetingMessage] = useState(bewertungen[0]);
   const [copied, setCopied] = useState(false);
 
   const handleTextChange = (e) => {
@@ -25,11 +33,11 @@ function Bewertungen() {
       <h1>Bewertungen</h1>
       <div className='formatierung'>
         <label htmlFor="textArea">Hier wäre ein Vorschlag für die Bewertung für einen Kunden. Du kannst ihn aber auch anpassen und dann kopieren: </label>
-        <textarea
-          id="textArea"
-          value={greetingMessage}
-          onChange={handleTextChange}
-        />
+        <select className="custom-select" onChange={handleTextChange} value={greetingMessage}>
+          {bewertungen.map((bewertung, index) => (
+            <option key={index} value={bewertung}>{bewertung}</option>
+          ))}
+        </select>
         <div className="emoji-buttons">
           <button className={copied ? "copy copied" : "copy"} onClick={copyToClipboard}>
             {copied ? "Kopiert!" : "Kopieren"}
