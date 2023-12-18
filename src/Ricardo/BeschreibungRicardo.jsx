7d98b157b35d1561aa30b48357c4a10fa2bf2c7f
Bernhardiner07 <timo.blumer@gmx.ch>
2023-12-18 21:42:55 +0100

    wow

--- src/Ricardo/BeschreibungRicardo.jsx
+++ src/Ricardo/BeschreibungRicardo.jsx
@@ -5,16 +5,18 @@ class BeschreibungRicardo extends Component {
   constructor(props) {
     super(props);
 
-    this.beschreibungRef = React.createRef();
     this.artikelDetailsRef = React.createRef();
-    this.oeffnungszeitenRef = React.createRef();
 
     this.state = {
       beschreibung: '',
       artikelDetails: '',
-      // Remove oeffnungszeiten from the state since it's hardcoded
     };
   }
+
+  handleCombinedButtonClick = () => {
+    this.handleHinweisEntfernen();
+    this.handleLeereZeilenLoeschen();
+  };
 
   handleHinweisEntfernen = () => {
     this.setState((prevState) => ({
@@ -23,15 +25,31 @@ class BeschreibungRicardo extends Component {
   };
 
   handleLeereZeilenLoeschen = () => {
-    this.setState((prevState) => ({
-      beschreibung: prevState.beschreibung.replace(/\n\s*\n/g, '\n'),
-      artikelDetails: prevState.artikelDetails.replace(/\n\s*\n/g, '\n'),
-    }));
+    this.setState((prevState) => {
+      // Remove the top empty line in beschreibung
+      let beschreibung = prevState.beschreibung.replace(/^\s*\n/, '');
+
+      // Remove the top empty line in artikelDetails
+      let artikelDetails = prevState.artikelDetails.replace(/^\s*\n/, '');
+
+      return {
+        beschreibung,
+        artikelDetails,
+      };
+    });
   };
 
   handleZusammenkopieren = () => {
     const { beschreibung, artikelDetails } = this.state;
-    const zusammengefuegt = `${beschreibung} \n\n${artikelDetails} \n\nProjekt Restwert Schönbühl\nÖffnungszeiten: Montag - Freitag 08:00 - 12:00 Uhr und 13:00 - 16:30 Uhr`;
+
+    // Artikeldetails als Liste formatieren
+    const artikelDetailsListe = artikelDetails
+      .split('\n')
+      .map((detail) => `• ${detail.trim()}`)
+      .join('\n');
+
+    // Text zusammenfügen
+    const zusammengefuegt = `${beschreibung} \n\n${artikelDetailsListe} \n\nProjekt Restwert Schönbühl\nÖffnungszeiten: Montag - Freitag 08:00 - 12:00 Uhr und 13:00 - 16:30 Uhr`;
 
     // Zugriff auf das DOM-Element
     const textarea = document.createElement('textarea');
@@ -50,24 +68,22 @@ class BeschreibungRicardo extends Component {
       <div className="BeschreibungRicardo">
         <textarea
           placeholder="Beschreibung"
-          className='beschreibung'
+          className="beschreibung"
           value={this.state.beschreibung}
           onChange={(e) => this.setState({ beschreibung: e.target.value })}
         />
         <textarea
           ref={this.artikelDetailsRef}
-          className='artikeldetails'
+          className="artikeldetails"
           placeholder="Artikeldetails"
           value={this.state.artikelDetails}
           onChange={(e) => this.setState({ artikelDetails: e.target.value })}
         />
-        <button onClick={this.handleHinweisEntfernen}>Hinweis entfernen!</button>
-        <button onClick={this.handleLeereZeilenLoeschen}>Leere Zeilen löschen</button>
-        <p>
-          Projekt Restwert Schönbühl
-          <br />
-          Öffnungszeiten: Montag - Freitag 08:00 - 12:00 Uhr und 13:00 - 16:30 Uhr
-        </p>
+        <button onClick={this.handleCombinedButtonClick}>Hinweis entfernen & Leere Zeilen löschen</button>
+        <div>
+          <p>Projekt Restwert Schönbühl</p>
+          <p>Öffnungszeiten: Montag - Freitag 08:00 - 12:00 Uhr und 13:00 - 16:30 Uhr</p>
+        </div>
         <button onClick={this.handleZusammenkopieren}>Zusammenkopieren</button>
       </div>
     );
