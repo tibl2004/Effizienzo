fcbbc1d0f98bd421bec0d3b1f32cdff109e4d931
Timo Blumer <timo.blumer@gewa.ch>
2024-02-06 23:03:27 +0100

    update

--- src/Erfassen/Erfassen.jsx
+++ src/Erfassen/Erfassen.jsx
@@ -1,126 +1,263 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Erfassen.scss';
+import React, { useState, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy, faTrash, faFilePdf, faCheck, faQuestionCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import jsPDF from 'jspdf';
+import '../Erfassen/Erfassen.scss';
+import Navbar from '../Navbar/Navbar';
+import axios from 'axios'; // Hier wurde der Import von 'axios' korrigiert
 
 function Erfassen() {
-  const [labels, setLabels] = useState([]);
-  const [inputData, setInputData] = useState({});
-  const [showInput, setShowInput] = useState(false);
-  const [newData, setNewData] = useState('');
-  const [editingId, setEditingId] = useState(null); // ID des Eintrags, der bearbeitet wird
-  const [editedText, setEditedText] = useState(''); // Aktuell bearbeiteter Text
+  const [erfassen, setErfassen] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:4000/erfassen');
-        setLabels(response.data);
-      } catch (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-      }
-    };
-
-    fetchData();
-
-    // Daten alle 5 Sekunden aktualisieren
-    const intervalId = setInterval(fetchData, 1000);
-
-    return () => clearInterval(intervalId); // Clear Interval beim Entfernen der Komponente
-  }, []); // Leerer Abhängigkeitsarray stellt sicher, dass der Effekt nur einmal ausgeführt wird
-
-  const handleInputChange = (id, value) => {
-    setInputData((prevData) => ({
-      ...prevData,
-      [id]: value,
-    }));
-  };
-
-  const handleDeleteInput = async (id) => {
-    try {
-      await axios.delete(`http://localhost:4000/erfassen/${id}`);
-      const updatedInputData = { ...inputData };
-      delete updatedInputData[id];
-      setInputData(updatedInputData);
-    } catch (error) {
-      console.error('Fehler beim Löschen der Daten:', error);
-      alert('Fehler beim Löschen der Daten!');
-    }
-  };
-
-  const handleEditInput = (id, text) => {
-    setEditingId(id);
-    setEditedText(text);
-  };
-
-  const handleSaveEdit = async (id) => {
-    try {
-      await axios.put(`http://localhost:4000/erfassen/${id}`, {
-        label: editedText
+    axios.get('http://localhost:4000/erfassen')
+      .then(response => {
+        setErfassen(response.data);
+      })
+      .catch(error => {
+        console.error('Fehler beim Abrufen der Updates:', error);
       });
-      setEditingId(null);
-    } catch (error) {
-      console.error('Fehler beim Speichern der Änderungen:', error);
-      alert('Fehler beim Speichern der Änderungen!');
+  }, []);
+
+  const [data, setData] = useState({
+    'Bei Nichtverkauf': '',
+    'Minimumpreis': '',
+    'Startpreis': '',
+    '1. Reaktivierung': '',
+    '2. Reaktivierung': '',
+    '3. Reaktivierung': '',
+  });
+
+  const [selectedOption, setSelectedOption] = useState('');
+  const [copiedLabel, setCopiedLabel] = useState(null);
+
+  const pdfRef = useRef(); // Ref für das PDF-Element
+
+  const handleInputChange = (label, value) => {
+    if (/^[0-9.]*$/.test(value) || label !== 'Startpreis') {
+      setData(prevData => ({
+        ...prevData,
+        [label]: value,
+      }));
     }
   };
 
-  const handlePostData = async () => {
+  const handleMinimumpreisChange = (label, value) => {
+    if (/^[0-9.]*$/.test(value) || label !== 'Minimumpreis') {
+      setData(prevData => ({
+        ...prevData,
+        [label]: value,
+      }));
+    }
+  };
+
+  const handleReaktivierungChange = (label, value) => {
+    if (/^[0-9.]*$/.test(value) || label !== '1. Reaktivierung') {
+      setData(prevData => ({
+        ...prevData,
+        [label]: value,
+      }));
+    }
+  };
+
+  const handleReaktivierung2Change = (label, value) => {
+    if (/^[0-9.]*$/.test(value) || label !== '2. Reaktivierung') {
+      setData(prevData => ({
+        ...prevData,
+        [label]: value,
+      }));
+    }
+  };
+
+  const handleReaktivierung3Change = (label, value) => {
+    if (/^[0-9.]*$/.test(value) || label !== '3. Reaktivierung') {
+      setData(prevData => ({
+        ...prevData,
+        [label]: value,
+      }));
+    }
+  };
+
+  const handleCopy = label => {
+    let text = `${label}: ${data[label]}`;
+
+    if (label === 'Minimumpreis' || label === 'Startpreis' || label === '1. Reaktivierung' || label === '2. Reaktivierung' || label === '3. Reaktivierung') {
+      text = `CHF ${data[label]}`;
+    }
+
     try {
-      await axios.post('http://localhost:4000/erfassen', {
-        label: newData
-      });
-      setNewData('');
-      setShowInput(false);
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+
+      setCopiedLabel(label);
+      setTimeout(() => setCopiedLabel(null), 1000);
     } catch (error) {
-      console.error('Fehler beim Posten der Daten:', error);
-      alert('Fehler beim Posten der Daten!');
+      console.error('Kopieren fehlgeschlagen:', error);
     }
   };
 
-  const handlePlusButtonClick = () => {
-    const newDataInput = prompt('Bitte geben Sie die Daten ein:');
-    if (newDataInput !== null) {
-      setNewData(newDataInput);
-      setShowInput(true);
+  const handleCopyBeiNichtverkauf = () => {
+    const text = `${selectedOption}`;
+
+    try {
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+
+      setCopiedLabel('Bei Nichtverkauf');
+      setTimeout(() => setCopiedLabel(null), 1000);
+    } catch (error) {
+      console.error('Kopieren fehlgeschlagen:', error);
     }
+  };
+
+  const handleClearAll = () => {
+    setData({
+      'Bei Nichtverkauf': '',
+      'Minimumpreis': '',
+      'Startpreis': '',
+      '1. Reaktivierung': '',
+      '2. Reaktivierung': '',
+      '3. Reaktivierung': '',
+    });
+  };
+
+  const handleExportToPDF = () => {
+    const doc = new jsPDF();
+    let yPos = 20;
+
+    Object.entries(data).forEach(([label, value]) => {
+      doc.text(20, yPos, `${label}: ${value}`);
+      yPos += 10;
+    });
+
+    doc.text(20, yPos, `Bei Nichtverkauf: ${selectedOption}`);
+    yPos += 10;
+
+    doc.save('erfassen.pdf');
   };
 
   return (
-    <div className="erfassen">
-      <ul>
-        {labels.map((item) => (
-          <li key={item.id}>
-            {editingId === item.id ? (
-              <>
-                <input
-                  type="text"
-                  value={editedText}
-                  onChange={(e) => setEditedText(e.target.value)}
+    <div className="erfassen-container">
+      <div className="warning-box">
+        <FontAwesomeIcon icon={faExclamationTriangle} style={{ color: 'white', marginRight: '5px' }} />
+        Bitte sonst noch andere wichtige relevante Informationen erfassen! Wie z.B. für technische Geräte ist es sehr wichtig!
+      </div>
+
+      <div className='button-container'>
+        <button
+          onClick={handleClearAll}
+          style={{
+            backgroundColor: 'red',
+            color: 'white',
+          }}
+        >
+          <span className="trash-icon">
+            <FontAwesomeIcon icon={faTrash} /> Alle Eingaben Löschen
+          </span>
+        </button>
+
+        <button onClick={handleExportToPDF} className="export-button">
+          <FontAwesomeIcon icon={faFilePdf} /> PDF Exportieren
+        </button>
+      </div>
+
+      <div className="input-container">
+        {erfassen.map(erfassen => (
+          <div key={erfassen.id} className="input-field">
+            <label>{erfassen.label}:</label>
+            <input
+              type="text"
+              value={data[erfassen.label]}
+              onChange={(e) => handleInputChange(erfassen.label, e.target.value)}
+            />
+            <button
+              onClick={() => handleCopy(erfassen.label)}
+              style={{
+                backgroundColor: copiedLabel === erfassen.label ? 'limegreen' : '',
+              }}
+            >
+              {copiedLabel === erfassen.label ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faCopy} />}
+            </button>
+          </div>
+        ))}
+        {Object.entries(data).map(([label, value]) => (
+          <div key={label} className="input-field">
+            <label style={{ fontSize: '18px', marginRight: '10px' }}>
+              {label}:
+              {label === 'Hinweise' && (
+                <FontAwesomeIcon
+                  icon={faQuestionCircle}
+                  style={{ marginLeft: '5px', color: '#007bff', cursor: 'pointer' }}
+                  title="Sonstige Weiterführende Informationen hier eintragen!!"
                 />
-                <button onClick={() => handleSaveEdit(item.id)}>Speichern</button>
+              )}
+            </label>
+            {label === 'Bei Nichtverkauf' ? (
+              <>
+                <select
+                  value={selectedOption}
+                  onChange={(e) => setSelectedOption(e.target.value)}
+                  className='select'
+                >
+                  <option value="">Auswählen</option>
+                  <option value="Spenden">Spenden</option>
+                  <option value="Rücknahme">Rücknahme</option>
+                </select>
+                <button
+                  onClick={handleCopyBeiNichtverkauf}
+                  style={{
+                    backgroundColor: copiedLabel === 'Bei Nichtverkauf' ? 'limegreen' : '',
+                  }}
+                >
+                  {copiedLabel === 'Bei Nichtverkauf' ? (
+                    <FontAwesomeIcon icon={faCheck} />
+                  ) : (
+                    <FontAwesomeIcon icon={faCopy} />
+                  )}
+                </button>
               </>
             ) : (
               <>
-                {item.label}:
                 <input
-                  type="text"
-                  value={inputData[item.id] || ''}
-                  onChange={(e) => handleInputChange(item.id, e.target.value)}
-                  disabled={editingId !== null} // Eingabefeld deaktivieren, wenn bearbeitet wird
+                  type={(label === 'Minimumpreis' || label === 'Startpreis' || label === '1. Reaktivierung' || label === '2. Reaktivierung' || label === '3. Reaktivierung') ? 'number' : 'text'}
+                  value={value}
+                  onChange={(e) => {
+                    if (label === 'Minimumpreis') {
+                      handleMinimumpreisChange(label, e.target.value);
+                    } else if (label === 'Startpreis') {
+                      handleInputChange(label, e.target.value);
+                    } else if (label === '1. Reaktivierung') {
+                      handleReaktivierungChange(label, e.target.value);
+                    } else if (label === '2. Reaktivierung') {
+                      handleReaktivierung2Change(label, e.target.value);
+                    } else if (label === '3. Reaktivierung') {
+                      handleReaktivierung3Change(label, e.target.value);
+                    } else {
+                      handleInputChange(label, e.target.value);
+                    }
+                  }}
                 />
-                <button onClick={() => handleEditInput(item.id, inputData[item.id] || '')}>Bearbeiten</button>
-                <button onClick={() => handleDeleteInput(item.id)}>Löschen</button>
+                <button
+                  onClick={() => handleCopy(label)}
+                  style={{
+                    backgroundColor: copiedLabel === label ? 'limegreen' : '',
+                  }}
+                >
+                  {copiedLabel === label ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faCopy} />}
+                </button>
               </>
             )}
-          </li>
+          </div>
         ))}
-      </ul>
-      {showInput && (
-        <div>
-          <button onClick={handlePostData}>Daten posten</button>
-        </div>
-      )}
-      <button onClick={handlePlusButtonClick}>+</button>
+      </div>
     </div>
   );
 }
